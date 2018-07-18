8348c66fce79420d0930f48c3af16c2b8d3848e0
515hikaru <karanodeny@gmail.com>
2018-07-19 00:47:33 +0900

    update emacs config

--- config/emacs.d/init.el
+++ config/emacs.d/init.el
@@ -4,7 +4,7 @@
         ("melpa" . "http://melpa.org/packages/")
         ("org" . "http://orgmode.org/elpa/")))
 (package-initialize)
-;;; (package-refresh-contents)
+(package-refresh-contents)
 ;;; basic config
 (setq inhibit-startup-message t)
 (setq make-backup-files nil)
@@ -27,8 +27,7 @@
 (blink-cursor-mode 0)
 (global-hl-line-mode t)
 (show-paren-mode 1)
-;;; theme
-(package-install 'atom-dark-theme)
+;;; Theme
 (require 'atom-one-dark-theme)
 ;;; Font
 (let* ((size 15)
@@ -46,20 +45,21 @@
 ;;; shell path exec
 (package-install 'exec-path-from-shell)
 (when (memq window-system '(mac ns x))
-(exec-path-from-shell-initialize))
-;; Install Intero
-(package-install 'intero)
-(add-hook 'haskell-mode-hook 'intero-mode)
+  (exec-path-from-shell-initialize))
+;;; open junk file
+(require 'open-junk-file)
+(global-set-key "\C-xj" 'open-junk-file)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(haskell-stylish-on-save t)
+ '(custom-safe-themes
+   (quote
+    ("78496062ff095da640c6bb59711973c7c66f392e3ac0127e611221d541850de2" default)))
  '(package-selected-packages
    (quote
-    (exec-path-from-shell exex-path-from-shell atom-dark-theme atom-one-dark-theme atom-one-dark intero))))
-
+    (atom-dark-theme open-junk-file atom-one-dark-theme exec-path-from-shell))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
