cf966f634cea83a0b752bc75381fce172d47cedf
515hikaru <karanodeny@gmail.com>
2018-07-20 00:48:52 +0900

    不要なファイルを削除

--- config/nvim/main.rs
+++
@@ -1,8 +0,0 @@
-use std::io;
-
-fn main() {
-    println!("Hello, World!!");
-    let mut foo = String::new();
-    io::stdin().read_line(&mut foo).expect("cannot read lines");
-    println!("foo is {}", foo);
-}
